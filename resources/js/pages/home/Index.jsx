848bf104a7e96d2206c02a388c7e9bcbb0fbd705
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-18 20:52:32 -0400

    Added test welcome message

--- resources/js/pages/home/Index.jsx
+++ resources/js/pages/home/Index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-function Index() {
+function Index({ user }) {
+
+    const welcomeMessage = () => {
+        const fname = user.fname ?? 'Guest';
+        if(fname !== 'Guest') {
+            return `Welcome ${fname}, here are the current conditions for this area`; 
+        }
+        return 'The current conditions for this area';
+    }
 
     return (
-        <>
-        
-        </>
+        <div>
+            <h2 className="text-center">{welcomeMessage()}</h2>
+            
+        </div>
     );
 }        
 export default Index;

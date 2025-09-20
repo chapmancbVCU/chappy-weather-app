6bc9156108ec6e396c387dd7e5257bc9cc04d63b
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-19 21:01:43 -0400

    Figured out how to print geo location data to console

--- resources/js/pages/home/Index.jsx
+++ resources/js/pages/home/Index.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CurrentConditions from "@/components/CurrentConditions";
+import geoLoc from "@/utils/geoLocation";
+
 function Index({ user }) {
-
     const welcomeMessage = () => {
-
+        
     }
+    
+    const [data, setData] = useState(null);
+    useEffect(() =>{
+        // const result = await geoLoc();
+        // setData(result);
+        geoLoc()
+            .then(d => {
+                setData(d)
+            })
+    }, []) 
 
+    console.log(data);
     return (
         <div>
             <h2 className="text-center">{welcomeMessage()}</h2>

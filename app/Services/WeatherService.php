52227e2421666a6da393feb7280e43a5bdbd780f
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-22 17:53:08 -0400

    Added constants for api call modes and function to test if it is valid

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -2,20 +2,24 @@
 namespace App\Services;
 
 use Core\Lib\Http\Api;
+use InvalidArgumentException;
 
 /**
  * Service that supports retrieving weather for current conditions.
  */
 class WeatherService extends Api {
+    private const GEO_LOCATE = 'http://api.openweathermap.org/geo/1.0';
+    private const ONE_CALL = 'http://api.openweathermap.org/data/3.0';
     private const STANDARD = 'http://api.openweathermap.org/data/2.5';
-
     /**
      * Setup instance of this class.  Configures default query with 
      * appid and units for suggestions to be returned.
      */
-    public function __construct(bool $oneCall = false) {
+    public function __construct(string $mode = self::STANDARD) {
+        
+        self::isValidMode($mode);
         parent::__construct(
-            baseUrl: self::STANDARD,
+            baseUrl: $mode,
             cacheNamespace: 'owm',
             defaultHeaders: ['Accept' => 'application/json'],
             defaultQuery: [
@@ -38,5 +42,17 @@ class WeatherService extends Api {
         $allowed = ['q', 'zip', 'lat', 'lon', 'units', 'lang'];
         $params = array_intersect_key($query, array_flip($allowed));
         return $this->get('/weather', $params);
+    }
+
+    /**
+     * Determines if mode provided in constructor is valid value
+     *
+     * @param string $mode The mode that determines appropriate API call.
+     * @return void
+     */
+    private function isValidMode(string $mode): void {
+        if(!in_array($mode, [self::GEO_LOCATE, self::ONE_CALL, self::STANDARD])) {
+            throw new InvalidArgumentException("Invalid api call: $mode");
+        }
     }
 }

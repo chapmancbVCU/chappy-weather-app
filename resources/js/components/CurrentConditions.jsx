50101805a3d9a0266536f271a6600693d14cc2ca
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-21 20:55:25 -0400

    Added jsdoc to describe CurrentConditions function

--- resources/js/components/CurrentConditions.jsx
+++ resources/js/components/CurrentConditions.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+
+/**
+ * Renders current conditions.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The current conditions for selected area.
+ */
 function CurrentConditions({ conditions, loading, error, units = 'imperial'}) {
     if(loading) return <div>Loading...</div>
     if(error) return <div className="text-danger">{error.message}</div>
-    
+
     return (
         <div className="card p-3">
       <h5 className="mb-2">{conditions.name}</h5>

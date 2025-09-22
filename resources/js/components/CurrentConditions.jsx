be6080e59e1d474aa5169a5fea59b5be84cbd8a2
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-21 20:32:45 -0400

    Removed default value for city prop

--- resources/js/components/CurrentConditions.jsx
+++ resources/js/components/CurrentConditions.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { apiGet, useAsync } from '@chappy/utils/api';
-function CurrentConditions({ city = 'Newport News, Virginia', units = 'imperial'}) {
+function CurrentConditions({ city, units = 'imperial'}) {
     const { data, loading, error} = useAsync(({ signal}) => 
         apiGet('/weather/currentConditions', { query: {q: city, units}, signal}),
     [city, units]);

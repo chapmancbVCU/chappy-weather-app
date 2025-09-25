8edb4d7bea92b6f1d4f01cb6a51f36a4ee4f403a
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-24 21:20:41 -0400

    Setup test daily view

--- resources/js/pages/daily/Index.jsx
+++ resources/js/pages/daily/Index.jsx
@@ -1,9 +1,36 @@
-import React from "react";
-function Index() {
+import React, { useEffect, useState, useMemo } from "react";
+import CurrentConditions from "@/components/CurrentConditions";
+import SearchBar from "@/components/SearchBar";
+import { Weather } from "@/utils/Weather";
+import { apiGet, useAsync } from '@chappy/utils/api';
+import useWeather from "@/utils/useWeather";
+/**
+ * Renders and handles information for daily forecast at a specific 
+ * location and search.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The view that displays daily forecast.
+ */
+function Index({ user }) {
+    const weather = useMemo(() => new Weather(), []);
+    const { city, units, onSubmit, currentError, currentLoading, current, hourly } = useWeather(weather);
 
+    const welcomeMessage = () => {
+        
+    }
+    // if(error) return <div className="text-danger">{error.message}</div>
     return (
         <>
-        hi
+            <SearchBar onSubmit={onSubmit}/>
+            <h2 className="text-center">Conditions in {city}</h2>
+            <CurrentConditions 
+                city={city} 
+                error={currentError} 
+                loading={currentLoading }
+                conditions={current} 
+                units={units} 
+            />
+            <div>Current lon: {current.coord?.lon}</div>
+            <div>Hourly lon: {hourly.lon}</div>
         </>
     );
 }        

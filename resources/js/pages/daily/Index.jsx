c31a2e60cdaa8f9b807785528485dab35aeef833
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-25 18:28:31 -0400

    Removed usage for CurrentConditions component

--- resources/js/pages/daily/Index.jsx
+++ resources/js/pages/daily/Index.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
-import CurrentConditions from "@/components/CurrentConditions";
 import SearchBar from "@/components/SearchBar";
 import { Weather } from "@/utils/Weather";
 import { apiGet, useAsync } from '@chappy/utils/api';
 import useWeather from "@/utils/useWeather";
+
 /**
  * Renders and handles information for daily forecast at a specific 
  * location and search.
@@ -26,19 +26,12 @@ function Index({ user }) {
 
     if(oneCallError) return <div className="text-danger">{oneCallError.message}</div>
     if(oneCallLoading) return <div>Loading...</div>
-    
+
     return (
         <>
             <SearchBar onSubmit={onSubmit}/>
             <div className="mt-3">
                 <h2 className="text-center">Conditions in {city}</h2>
-                <CurrentConditions 
-                    city={city} 
-                    error={currentError} 
-                    loading={currentLoading }
-                    conditions={current} 
-                    units={units} 
-                />
                 <div>Current lon: {current.coord?.lon}</div>
                 <div>OneCall lon: {oneCall.lon}</div>
             </div>
--- resources/js/pages/hourly/Index.jsx
+++ resources/js/pages/hourly/Index.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
-import CurrentConditions from "@/components/CurrentConditions";
 import SearchBar from "@/components/SearchBar";
 import { Weather } from "@/utils/Weather";
 import { apiGet, useAsync } from '@chappy/utils/api';
 import useWeather from "@/utils/useWeather";
+
 /**
  * Renders and handles information for hourly conditions at a specific 
  * location and search.
@@ -33,13 +33,6 @@ function Index({ user }) {
             <SearchBar onSubmit={onSubmit}/>
             <div className="mt-3">
                 <h2 className="text-center">Conditions in {city}</h2>
-                <CurrentConditions 
-                    city={city} 
-                    error={currentError} 
-                    loading={currentLoading }
-                    conditions={current} 
-                    units={units} 
-                />
                 <div>Current lon: {current.coord?.lon}</div>
                 <div>OneCall lon: {oneCall.lon}</div>
             </div>

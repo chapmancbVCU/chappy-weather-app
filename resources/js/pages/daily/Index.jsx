39482da3834fc4143e29e3c5a8b50b590e5ed65a
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-25 19:08:59 -0400

    Use error component for rendering errors

--- resources/js/pages/daily/Index.jsx
+++ resources/js/pages/daily/Index.jsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { Weather } from "@/utils/Weather";
 import useWeather from "@/utils/useWeather";
 import DailyForecast from "@/components/DailyForecastCard";
+import Error from "@/components/Error";
 
 /**
  * Renders and handles information for daily forecast at a specific 
@@ -29,9 +30,10 @@ function Index({ user }) {
     return (
         <>
             <SearchBar onSubmit={onSubmit}/>
-            {currentError ? (
-                <div className="text-danger text-center mt-3">{currentError.message}</div>
-            ) : ( 
+            {currentError && <Error error={currentError} />}
+            {oneCallError && <Error error={oneCallError} />}
+
+            {!currentError && !oneCallError && ( 
                 <div className="mt-3">
                     <h2 className="text-center">Daily forecast for {city}</h2>
                     <div>Current lon: {current.coord?.lon}</div>

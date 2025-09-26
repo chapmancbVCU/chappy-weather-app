86830fcd8d6886335fcb6b35cf3e287a21b38ecd
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-25 20:17:19 -0400

    Begin styling of switch

--- resources/js/components/UnitsSwitch.jsx
+++ resources/js/components/UnitsSwitch.jsx
@@ -1,23 +1,26 @@
 import React from "react";
-
+import cx from "classnames";
 /**
  * @property {string} units The name of the system of units.
  * @param {InputProps} param0 
  * @returns 
  */
 function UnitsSwitch({ units }) {
-
-
+    const rounded = true;
+    const sliderCX = cx('slider', { 'rounded': rounded});
     const label = (units === 'imperial') ? 'F' : 'C'; 
     return (
         <div className="toggle-switch-container me-3">
             <h2>{'\xB0'}{label}</h2>
-            <input type="checkbox"
-                name="isToggled"
-                id="isToggled"
-                // checked={""}
-                // onChange={""}
-            />
+            <label className="toggle-switch">
+                <input type="checkbox"
+                    name="isToggled"
+                    id="isToggled"
+                    // checked={""}
+                    // onChange={""}
+                />
+                <span className={sliderCX}></span>
+            </label>
         </div>
     );
 }        

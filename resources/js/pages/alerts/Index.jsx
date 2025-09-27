73a93a984103548d3f3d85963f001b6e0cbd13f5
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 18:18:02 -0400

    Added jsdoc

--- resources/js/pages/alerts/Index.jsx
+++ resources/js/pages/alerts/Index.jsx
@@ -14,6 +14,7 @@ import AlertCard from "@/components/AlertCard";
 function Index() {
 
     const weather = useMemo(() => new Weather(), []);
+    
     const { 
         city, 
         currentError, 

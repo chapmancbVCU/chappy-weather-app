450c72eecfdfd30b72a3972cddc038d1b3078d5a
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 19:29:23 -0400

    Added hook to clean up code

--- resources/js/components/CurrentConditions.jsx
+++ resources/js/components/CurrentConditions.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@css/forecast.css";
+import useCurrentConditions from "@/utils/useCurrentConditions";
 
 /**
  * Renders current conditions.
@@ -7,9 +8,14 @@ import "@css/forecast.css";
  * @returns {JSX.Element} The current conditions for selected area.
  */
 function CurrentConditions({ conditions, oneCall, units}) {
+    const { summary } = useCurrentConditions(conditions, oneCall);
+
     return (
         <div className="card forecast mt-4">
-            <h4 className="text-center my-3">{oneCall.daily[0].summary}</h4>
+            <h4 className="text-center my-3">{summary}</h4>
+            <div>
+
+            </div>
         </div>
     );
 }        

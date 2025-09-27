7236e4175882f5b917a299f06ced9cdd4c78b217
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 16:03:49 -0400

    Begin basic setup of view

--- resources/js/pages/alerts/Index.jsx
+++ resources/js/pages/alerts/Index.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useMemo } from "react";
+import SearchBar from "@/components/SearchBar";
+import { Weather } from "@/utils/Weather";
+import useWeather from "@/utils/useWeather";
+import Error from "@/components/Error";
+
 function Index() {
+
+    const weather = useMemo(() => new Weather(), []);
+    const { 
+        city, 
+        current,
+        currentError, 
+        currentLoading, 
+        handleToggleChange, 
+        isToggled, 
+        oneCall,
+        oneCallError,
+        oneCallLoading,
+        onSubmit, 
+        units, 
+        unitsLabel, 
+    } = useWeather(weather);
+
+    if(currentLoading || oneCallLoading) return <div className="mt-3 text-center">Loading...</div>
 
     return (
         <>
-        
+            <SearchBar onSubmit={onSubmit}/>
+            {currentError && <Error error={currentError} />}
+            {oneCallError && <Error error={oneCallError} />}
+
+            {!currentError && !oneCallError && (
+                <div className="mt-3 d-flex flex-column">
+                    <h1 className="text-center">Alerts in your area</h1>
+                </div>
+            )}
         </>
     );
 }        

cbd82607c071a53a14c097f0146b8bcbe0366d05
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 15:34:49 -0400

    Removed error and loading

--- resources/js/components/CurrentConditions.jsx
+++ resources/js/components/CurrentConditions.jsx
@@ -6,9 +6,6 @@ import React from "react";
  * @returns {JSX.Element} The current conditions for selected area.
  */
 function CurrentConditions({ conditions, units = 'imperial'}) {
-    if(loading) return <div>Loading...</div>
-    if(error) return <div className="text-danger">{error.message}</div>
-
     return (
         <div className="card p-3">
             <h5 className="mb-2">{conditions.name}</h5>

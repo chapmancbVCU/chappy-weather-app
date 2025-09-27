c297fef5a76b8388b1308155cd74525f085fe2e0
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 15:49:28 -0400

    Added phpdoc to file

--- app/Controllers/AlertsController.php
+++ app/Controllers/AlertsController.php
@@ -3,9 +3,14 @@ namespace App\Controllers;
 use Core\Controller;
 
 /**
- * Undocumented class
+ * Supports operations for rendering alerts view.
  */
 class AlertsController extends Controller {
+    /**
+     * Renders alerts at specified location.
+     *
+     * @return void
+     */
     public function indexAction(): void {
         $this->view->renderJsx('alerts.Index');
     }

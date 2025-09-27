6494f0ce9faa221a24e71b623a65f31dd241264a
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-27 16:37:24 -0400

    Begin to build card for individual alert

--- resources/js/components/AlertCard.jsx
+++ resources/js/components/AlertCard.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-function AlertCard() {
+import React, { useState } from "react";
+import "@css/alerts.css";
+import { Card, Button, Collapse } from 'react-bootstrap'; 
+function AlertCard({ alert }) {
 
+    const [open, setOpen] = useState(false);
     return (
-        <>
-        
-        </>
+        <Card className="alert-card my-3">
+            <Card.Header>
+                <Button
+                    className="alert-button"
+                    onClick={() => setOpen(!open)}
+                    aria-controls="collapse-text"
+                    area-expand={open}
+                    variant="link"
+                >
+                    {alert.event}
+                </Button>
+            </Card.Header>
+            <Collapse in={open}>
+                <div id="collapse-text">
+                    <Card.Body>
+
+                    </Card.Body>
+                </div>
+            </Collapse>
+        </Card>
     );
 }        
 export default AlertCard;

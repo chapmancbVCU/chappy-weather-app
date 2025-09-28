b6a27f63ca358b3a59910d984e5e5acf07b61572
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-28 11:30:50 -0400

    Added jsdoc

--- resources/js/utils/DateTimeUtil.js
+++ resources/js/utils/DateTimeUtil.js
@@ -2,11 +2,18 @@
  * Supports the display of date and time information.
  */
 export class DateTimeUtil {
-
+    /**
+     * Returns the day of month using the ISO timestamp as parameter.
+     * @param {string} timestamp Date and time information in the form of an 
+     * ISO string.
+     * @returns {string} The day of month contained in ISO timestamp.
+     */
     getDayOfMonth(timestamp) {
         let dayOfMonth = timestamp?.toString().slice(5, 7);
         return (dayOfMonth < 10) 
             ? dayOfMonth?.slice(1,2)
             : dayOfMonth;
     }
+
+    
 }

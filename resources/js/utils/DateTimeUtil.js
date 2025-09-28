4d5b6d79991f6b36816e330bda7c1a2a5f3e0a84
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-28 11:28:45 -0400

    Created getDayOfMonth function

--- resources/js/utils/DateTimeUtil.js
+++ resources/js/utils/DateTimeUtil.js
@@ -3,4 +3,10 @@
  */
 export class DateTimeUtil {
 
+    getDayOfMonth(timestamp) {
+        let dayOfMonth = timestamp?.toString().slice(5, 7);
+        return (dayOfMonth < 10) 
+            ? dayOfMonth?.slice(1,2)
+            : dayOfMonth;
+    }
 }

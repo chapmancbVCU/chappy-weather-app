acd6f694779619764a2862fcfe3fd2f3132bef13
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-01 20:44:40 -0400

    Created main div and imported forecast.css

--- resources/js/components/DailyForecast.jsx
+++ resources/js/components/DailyForecast.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "@css/forecast.css";
 function DailyForecast({ oneCall, units }) {
 
     return (
-        <>
-        
-        </>
+        <div className="card forecast my4">
+
+        </div>
     );
 }        
 export default DailyForecast;

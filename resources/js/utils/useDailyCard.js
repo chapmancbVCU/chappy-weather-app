e0a2cb34ccc2d75f2e586ad682dae2415cee92be
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 18:24:33 -0400

    Initial commit

---
+++ resources/js/utils/useDailyCard.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState, useMemo } from "react";
+
+const useDailyCard = (oneCall) => {
+
+
+
+    useEffect(() => {
+
+    });
+    return {
+
+    }
+}
+
+export default useDailyCard;

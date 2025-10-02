89e351895c669bfddbaefeb17d0f6e523ec8fd3b
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 18:30:08 -0400

    Moved file

--- resources/js/utils/hooks/useDailyCard.js
+++ resources/js/utils/hooks/useDailyCard.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useMemo } from "react";
-
+import { DateTimeUtil } from "../DateTimeUtil";
 const useDailyCard = (oneCall) => {
-
-
+    const dateTimeUtil = useMemo(() => new DateTimeUtil(), []);
+    
 
     useEffect(() => {
 

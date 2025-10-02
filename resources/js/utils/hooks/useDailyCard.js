76448140d35eaf1c0fd84f6884ee94ad6d2a57ac
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 19:11:47 -0400

    Removed logging

--- resources/js/utils/hooks/useDailyCard.js
+++ resources/js/utils/hooks/useDailyCard.js
@@ -3,10 +3,8 @@ import { DateTimeUtil } from "../DateTimeUtil";
 const useDailyCard = (daily, tzOffset) => {
     const dateTimeUtil = useMemo(() => new DateTimeUtil(), []);
     const [date, setDate] = useState("");
+    
     useEffect(() => {
-        console.log(daily.dt);
-        console.log(tzOffset)
-
         const stamp = dateTimeUtil.getDateTime(daily.dt, tzOffset);
         setDate(dateTimeUtil.getForecastDate(stamp))
 

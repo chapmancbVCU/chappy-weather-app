8ccd06a65c813a221cc8266edbac6a4b7ad25b88
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 18:22:42 -0400

    Added container and map function call for dailyForecast

--- resources/js/components/DailyForecast.jsx
+++ resources/js/components/DailyForecast.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "@css/forecast.css";
+import useDaily from "@/utils/hooks/useDaily";
+import DailyForecastCard from "./DailyForecastCard";
+
 function DailyForecast({ oneCall, units }) {
 
+    const { dailyForecast } = useDaily(oneCall);
+    console.log(dailyForecast)
     return (
-        <div className="card forecast my4">
-
+        <div className="card forecast my-4">
+            <div className="daily-forecast-container">
+                {dailyForecast && dailyForecast.map((daily, index) => (
+                    <DailyForecastCard key={index}/>
+                ))}
+            </div>
         </div>
     );
 }        

a217307522941f26ba1a1d64ddd2959beb29fa7a
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 21:19:31 -0400

    Added jsdoc to describe hooks

--- resources/js/utils/hooks/useHourlyCard.js
+++ resources/js/utils/hooks/useHourlyCard.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { DateTimeUtil } from "../DateTimeUtil";
 
+/**
+ * Manages states and hooks for a particular daily forecast card.
+ * @param {object} daily The data for a particular day.
+ * @param {number} tzOffset The the timezone offset.
+ * @returns 
+ */
 const useHourlyCard = (hourly, tzOffset) => {
     const dateTimeUtil = useMemo(() => new DateTimeUtil(), []);
     const [date, setDate] = useState("");

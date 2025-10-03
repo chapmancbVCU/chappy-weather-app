95e1915f2981bdcc45447e12d84b82ac4f3d6032
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-02 21:10:39 -0400

    Added jsdoc to describe components

--- resources/js/components/DailyForecastCard.jsx
+++ resources/js/components/DailyForecastCard.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import "@css/forecast.css";
 import useDailyCard from "@/utils/hooks/useDailyCard";
 
+/**
+ * Renders daily forecast card component.
+ * 
+ * @property {object} daily The object for weather data from a particular day.
+ * @property {index} index The key provided by the map function from parent 
+ * component.
+ * @property {object} onCardClick The handler for when user clicks on a card.
+ * @property {object} tzOffset The the timezone offset.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The daily forecast card component.
+ */
 function DailyForecastCard({daily, index, onCardClick, tzOffset}) {
+    
     const {
         date
     } = useDailyCard(daily, tzOffset);
-    
+
     return (
         <button className="daily-forecast-card"
             onClick={() => onCardClick(index)}
--- resources/js/components/HourlyForecastCard.jsx
+++ resources/js/components/HourlyForecastCard.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import "@css/forecast.css";
 import useHourlyCard from "@/utils/hooks/useHourlyCard";
 
+/**
+ * Renders hourly forecast card component.
+ * 
+ * @property {object} hourly The object for weather data from a particular 
+ * hour.
+ * @property {index} index The key provided by the map function from parent 
+ * component.
+ * @property {object} onCardClick The handler for when user clicks on a card.
+ * @property {object} tzOffset The the timezone offset.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The hourly forecast card component.
+ */
 function HourlyForecastCard({ hourly, index, onCardClick, tzOffset }) {
     const {
         date

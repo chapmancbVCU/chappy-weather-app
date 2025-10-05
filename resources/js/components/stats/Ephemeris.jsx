2a5ca11752bf5bc11c5100789175454a75d18f20
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-05 16:22:08 -0400

    Added hooks, imported required items, and setup props

--- resources/js/components/stats/Ephemeris.jsx
+++ resources/js/components/stats/Ephemeris.jsx
@@ -1,5 +1,30 @@
-import React from "react";
-function Ephemeris() {
+import React, { useEffect, useState, useMemo } from "react";
+import { DateTimeUtil } from "../DateTimeUtil";
+function Ephemeris({ data, tzOffset }) {
+    const dateTimeUtil = useMemo(() => new DateTimeUtil(), []);
+    /**
+     * Sets message for moonrise.
+     * @type {[string, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [moonRise, setMoonRise] = useState("");
+
+    /**
+     * Sets message for moonset.
+     * @type {[string, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [moonSet, setMoonSet] = useState("");
+
+    /**
+     * Sets message for sunrise.
+     * @type {[string, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [sunRise, setSunRise] = useState("");
+
+    /**
+     * Sets message for sunset.
+     * @type {[string, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [sunSet, setSunSet] = useState("");
 
     return (
         <>

64936b935114993d3fcfececd92fd75423293d92
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-05 16:16:59 -0400

    Implemented humidity component

--- resources/js/components/stats/Humidity.jsx
+++ resources/js/components/stats/Humidity.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@css/forecast.css";
+import asset from "@chappy/utils/asset";
 
 /**
  * Renders information about humidity.
@@ -8,12 +9,19 @@ import "@css/forecast.css";
  * @param {InputProps} param0 
  * @returns {JSX.Element} Component containing humidity information.
  */
-function Humidity() {
-
+function Humidity({ data }) {
+    if (data == null) return;
+    
     return (
-        <>
-        
-        </>
+        <div className="forecast-info">
+            <div className="forecast-icon-container">
+                <img className="forecast-icon" src={asset('public/icons/UVI.png')} />
+            </div>
+            <div className="forecast-info-block">
+                Humidity
+                <div>{data} %</div>
+            </div>
+        </div>
     );
 }        
 export default Humidity;

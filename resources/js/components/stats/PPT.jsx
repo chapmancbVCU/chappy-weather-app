1f884b409d2b68e3762ed2bbbc4db15d46b496bc
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-05 18:27:44 -0400

    Implemented PPT

--- resources/js/components/stats/PPT.jsx
+++ resources/js/components/stats/PPT.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-function PPT() {
+import "@css/forecast.css";
+import asset from "@chappy/utils/asset";
+function PPT({ data }) {
 
     return (
-        <>
-        
-        </>
+        <div className="forecast-info">
+            <div className="forecast-icon-container">
+                <img className="forecast-icon" src={asset('public/icons/weather-pouring.png')} />
+            </div>
+            <div className="forecast-info-block">
+                Chance of PPT
+                <div>
+                    {(data * 100).toFixed()}%
+                </div>
+            </div>
+        </div>
     );
 }        
 export default PPT;

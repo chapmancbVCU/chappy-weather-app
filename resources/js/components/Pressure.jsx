7ad62e6947b315aac4bac0cf97b87bf4ec48aeb5
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-05 13:57:49 -0400

    Initial commit

---
+++ resources/js/components/Pressure.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import asset from "@chappy/utils/asset";
+import "@css/forecast.css";
+function Pressure() {
+
+    return (
+        <>
+        
+        </>
+    );
+}        
+export default Pressure;

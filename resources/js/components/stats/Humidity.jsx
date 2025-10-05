a1527c1498566123afa62d4529d22fb53ac4902e
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-05 16:13:49 -0400

    Made imports and added jsdoc

--- resources/js/components/stats/Humidity.jsx
+++ resources/js/components/stats/Humidity.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import "@css/forecast.css";
+
+/**
+ * Renders information about humidity.
+ * 
+ * @property {number} data The UVI data.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} Component containing humidity information.
+ */
 function Humidity() {
 
     return (

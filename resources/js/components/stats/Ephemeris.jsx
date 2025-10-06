30c2b05949205c12a44aa20bba9a5c13b81ff036
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-06 18:33:59 -0400

    Remove unused imports

--- resources/js/components/stats/Ephemeris.jsx
+++ resources/js/components/stats/Ephemeris.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import { DateTimeUtil } from "@/utils/DateTimeUtil";
 import asset from "@chappy/utils/asset";
 

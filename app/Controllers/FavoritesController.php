8c299a0106f9e7f04c13cc6c5435a16784e5a368
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 18:38:52 -0400

    Started showAction and retriee data for manageFavoritesAction

--- app/Controllers/FavoritesController.php
+++ app/Controllers/FavoritesController.php
@@ -1,12 +1,21 @@
 <?php
 namespace App\Controllers;
 use Core\Controller;
-
+use Core\Services\AuthService;
+use App\Models\Favorites;
 /**
  * Supports operations related to favorites.
  */
 class FavoritesController extends Controller {
     public function manageFavoritesAction(): void {
+        $user = AuthService::currentUser();
+        $favorites = Favorites::findAllByUserId($user->id);
+        $this->view->props = ['favorites' => $favorites];
         $this->view->renderJsx('favorites.ManageFavorites');
+    }
+
+    public function showAction(): void {
+        $user = AuthService::currentUser();
+        $favorites = Favorites::findAllByUserId($user->id);
     }
 }

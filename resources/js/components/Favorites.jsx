b7dc5ab10a53135d3f63416b89e9a76c9bfa0b62
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 17:20:06 -0400

    Added styling for parent favorites div

--- resources/js/components/Favorites.jsx
+++ resources/js/components/Favorites.jsx
@@ -2,9 +2,9 @@ import React from "react";
 function Favorites() {
 
     return (
-        <>
+        <div className="favorites-bar mx-auto">
             Favorites bar
-        </>
+        </div>
     );
 }        
 export default Favorites;

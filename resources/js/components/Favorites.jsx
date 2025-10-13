cdd01dd26886040316d0c1ec031f0a021984cdb4
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 18:22:48 -0400

    Added margin

--- resources/js/components/Favorites.jsx
+++ resources/js/components/Favorites.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Favorites() {
 
     return (
-        <div className="favorites-bar mx-auto">
+        <div className="favorites-bar mx-auto my-3">
             <h5 className="text-center my-2">Favorite Locations</h5>
             
         </div>

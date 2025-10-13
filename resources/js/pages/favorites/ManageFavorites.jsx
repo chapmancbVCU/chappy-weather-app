93368f4257e5177c629ddb5ede670df72699fb8d
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 19:30:51 -0400

    Added test content

--- resources/js/pages/favorites/ManageFavorites.jsx
+++ resources/js/pages/favorites/ManageFavorites.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-function ManageFavorites() {
-
+import documentTitle from "@chappy/utils/documentTitle";
+import "@css/forecast.css";
+function ManageFavorites({ favorites }) {
+    console.log(favorites);
+    documentTitle('Your Favorites');
     return (
-        <>
-            Manage favorites
-        </>
+        <div className="d-flex flex-column w-75 mx-auto">
+            <h1 className="text-center">Your Favorites</h1>
+            <div className="d-flex mx-auto flex-row ">
+                {favorites && favorites.map((favorite, index) => (
+                    <div className="favorites-card" key={index}>
+                        <h5 className="my-2">{favorite.name}</h5>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }        
 export default ManageFavorites;

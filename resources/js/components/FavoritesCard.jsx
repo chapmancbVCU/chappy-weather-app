bdcd7b452affe34b5ac7a047246c20a7522eb3fa
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 20:52:17 -0400

    Got API data to be retrieved, added logging, and added error component

--- resources/js/components/FavoritesCard.jsx
+++ resources/js/components/FavoritesCard.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { apiGet, useAsync } from '@chappy/utils/api';
+import { Weather } from "@/utils/Weather";
+import Error from "./Error";
 function FavoritesCard({ favorite }) {
+    const weather = useMemo(() => new Weather(), []);
+    const [units, setUnits] = useState("");
+
+    useEffect(() => {
+        setUnits(weather.getUnits());
+    })
+
+    const { 
+        data: favoriteData, 
+        loading: favoriteLoading, 
+        error: favoriteError} = useAsync(({ signal}) => {
+            if (!favorite) return Promise.resolve(null);
+            const u = units || "imperial";
+            return apiGet("/weather/currentConditions", { query: { q: favorite.name, units: u }, signal });
+    }, [favorite.name, units]);
+
+    if(favoriteLoading) return <div className="mt-3 text-center">Loading...</div>
+
+    console.log("*********************************")
+    console.log("Favorite card data");
+    console.log(favorite);
+    console.log(favoriteData);
+    console.log("*********************************")
 
     return (
-        <div className="manage-favorites-card">
-            <h5 className="my-2">{favorite.name}</h5>
-        </div>
+        <>
+            {favoriteError && <Error error={favoriteError} />}
+            {!favoriteError && 
+                <div className="manage-favorites-card">
+                    <h5 className="my-2">{favorite.name}</h5>
+                </div>
+            }
+        </>
     );
 }        
 export default FavoritesCard;

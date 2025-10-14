3d3b6d3bb7896818c140640e5fe1eefc8782da57
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-13 21:32:31 -0400

    Set temperature

--- resources/js/components/FavoritesCard.jsx
+++ resources/js/components/FavoritesCard.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useMemo, useState } from "react";
 import { apiGet, useAsync } from '@chappy/utils/api';
 import { Weather } from "@/utils/Weather";
 import Error from "./Error";
+import useTemperature from "@/utils/hooks/useTemperature";
+
 function FavoritesCard({ favorite }) {
     const weather = useMemo(() => new Weather(), []);
     const [units, setUnits] = useState("");
-
+    const { temperature } = useTemperature(units);
     useEffect(() => {
         setUnits(weather.getUnits());
     })
@@ -18,7 +20,7 @@ function FavoritesCard({ favorite }) {
             const u = units || "imperial";
             return apiGet("/weather/currentConditions", { query: { q: favorite.name, units: u }, signal });
     }, [favorite.name, units]);
-
+    const data = favoriteData?.data;
     if(favoriteLoading) return <div className="mt-3 text-center">Loading...</div>
 
     console.log("*********************************")
@@ -33,6 +35,7 @@ function FavoritesCard({ favorite }) {
             {!favoriteError && 
                 <div className="manage-favorites-card">
                     <h5 className="my-2">{favorite.name}</h5>
+                    <p>{temperature(data.main.temp)}</p>
                 </div>
             }
         </>

79e878686ea31a5d4d6c2ce7d957babd462a1006
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-14 20:47:47 -0400

    Added styling for container and made card width static

--- resources/js/components/Favorites.jsx
+++ resources/js/components/Favorites.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import FavoritesCard from "./FavoritesCard";
+import "@css/forecast.css";
 function Favorites({ favorites, units }) {
 
     return (
         <div className="favorites-bar mx-auto my-3">
             <h5 className="text-center my-2">Favorite Locations</h5>
-            <div className="d-flex flex-row">
+            <div className="favorite-cards-container">
                 {favorites && favorites.map((favorite, index) => (
                     <FavoritesCard favorite={favorite} key={index} units={units}/> 
                 ))}

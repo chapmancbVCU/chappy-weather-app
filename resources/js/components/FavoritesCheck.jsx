8070da966d931471b771cf74e420dba1371ef653
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-15 20:52:15 -0400

    Implemented logic to determine if location is a favorite

--- resources/js/components/FavoritesCheck.jsx
+++ resources/js/components/FavoritesCheck.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 function FavoritesCheck({ weather, favorites }) {
 
+    const [isFavorite, setIsFavorite] = useState(false);
     console.log("********************");
     console.log("Favorites Check");
     console.log(weather)
     console.log(favorites)
     console.log("********************");
+    const isFavoriteCity = () => {
+        const weatherCity = weather.city.toLowerCase();
+        for(let i = 0; i < favorites.length; i++) {
+            let favCity = favorites[i].name.toLowerCase();
+            if(favCity.includes(weatherCity) || weatherCity.includes(favCity)) {
+                setIsFavorite(true);
+                break;
+            }
+        }
+    }
+
+    useEffect(() => {
+
+        isFavoriteCity();
+    })
     return (
         <>
-            Checkbox
+            {isFavorite ? <p>Is favorite</p> : <p>Not favorite</p>}
         </>
     );
 }        

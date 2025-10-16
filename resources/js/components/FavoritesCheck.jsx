edd534f226ff196969f9eea2ae8bc29157ade91c
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-15 20:35:41 -0400

    Added favorites array to logging

--- resources/js/components/FavoritesCheck.jsx
+++ resources/js/components/FavoritesCheck.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 function FavoritesCheck({ weather, favorites }) {
+
     console.log("********************");
     console.log("Favorites Check");
     console.log(weather)
+    console.log(favorites)
     console.log("********************");
     return (
         <>

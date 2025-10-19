1acf7b4acd882a39e2b553db7bfb457787e270e1
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-19 19:00:12 -0400

    Added jsdoc

--- resources/js/utils/hooks/useFavorites.js
+++ resources/js/utils/hooks/useFavorites.js
@@ -1,5 +1,9 @@
 import { useAsync, apiGet } from "@chappy/utils/api";
 
+/**
+ * Obtains list favorites using the showAction of the FavoritesController.
+ * @returns 
+ */
 const useFavorites = () => {
     const { data, loading, error } = useAsync(({ signal}) => 
             apiGet('/favorites/show', { signal }))

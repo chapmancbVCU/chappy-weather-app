b033f3a52d8ee60e0f1692710e3774d7b65458d4
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-19 18:55:32 -0400

    Added jsdoc

--- resources/js/components/Favorites.jsx
+++ resources/js/components/Favorites.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import FavoritesCard from "./FavoritesCard";
 import "@css/forecast.css";
-function Favorites({ favorites, units }) {
 
+/**
+ * Renders a component containing favorites in a scrollable container.
+ * @property {array} favorites The favorites associated with currently logged 
+ * in user.
+ * @property {string} units The system of units in use.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The container with favorites.
+ */
+function Favorites({ favorites, units }) {
     return (
         <div className="favorites-bar mx-auto my-3">
             <h5 className="text-center my-2">Favorite Locations</h5>

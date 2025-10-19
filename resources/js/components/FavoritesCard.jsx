662cbd71a5744c2cb8031eafe2e9063c9671adb6
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-19 19:32:07 -0400

    Added event handler to refresh page with city associated with favorite button clicked

--- resources/js/components/FavoritesCard.jsx
+++ resources/js/components/FavoritesCard.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { apiGet, useAsync } from '@chappy/utils/api';
 import Error from "./Error";
 import useTemperature from "@/utils/hooks/useTemperature";
 import useIcon from "@/utils/hooks/useIcon";
 import useDescription from "@/utils/hooks/useDescription";
+import { Weather } from "@/utils/Weather";
+
 function FavoritesCard({ favorite, units }) {
+    const weather = useMemo(() => new Weather(), []);
     const { temperature } = useTemperature(units);
 
     const { 
@@ -21,18 +24,24 @@ function FavoritesCard({ favorite, units }) {
     const { description } = useDescription(data?.weather?.[0].description);
     const { icon } = useIcon(data?.weather?.[0]?.icon);
     
+    const onCardClick = () => {
+        console.log(favorite.name)
+        weather.updateStorage(data, units, favorite.name);
+        window.location.reload();
+    }
+
     if(favoriteLoading) return <div className="mt-3 text-center">Loading...</div>
 
     return (
         <>
             {favoriteError && <Error error={favoriteError} />}
             {!favoriteError && 
-                <div className="manage-favorites-card">
+                <button className="manage-favorites-card" onClick={() => onCardClick()}>
                     <h5 className="my-2">{favorite.name}</h5>
                     <p>{description}</p>
                     {icon && <img src={icon} alt={description}></img>}
                     <p>{temperature(data.main.temp)}</p>
-                </div>
+                </button>
             }
         </>
     );

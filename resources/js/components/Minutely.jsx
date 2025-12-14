776310f569adf0328a57b90f5b31388d260ccc6d
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-14 18:55:30 -0500

    Added jsdoc to describe component

--- resources/js/components/Minutely.jsx
+++ resources/js/components/Minutely.jsx
@@ -3,9 +3,14 @@ import "@css/forecast.css";
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
+/**
+ * Renders minutely precipitation forecast as a bar chart.
+ * @param {InputProps} param0 
+ * @returns {JSX.Element} The minutely precipitation forecast.
+ */
 function Minutely({ minutely }) {
     console.log(minutely);
-    
+
     return (
         <div className="card forecast my-4">
             <h4 className="text-center mt-4">Minutely Forecast</h4>

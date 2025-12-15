f5f683ce1d6695788b01be25a4c2f45b95bf13c7
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-14 21:06:48 -0500

    jsdoc styling update

--- resources/js/pages/error/NotFound.jsx
+++ resources/js/pages/error/NotFound.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 /**
  * Renders error page with "The view was not found message".
+ * 
  * @returns {JSX.Element} H1 element with "The view was not found" message.
  */
 export default function NotFound() {

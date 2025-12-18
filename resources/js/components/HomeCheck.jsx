cdf0d4dfe755159c8aa9777f001eca70f79d78e4
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-17 21:28:57 -0500

    Added necessary imports

--- resources/js/components/HomeCheck.jsx
+++ resources/js/components/HomeCheck.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-function HomeCheck() {
+import React, { useEffect, useState } from "react";
+import Forms from "@chappy/components/Forms";
+import { apiPost, apiError } from "@chappy/utils/api";
+
+function HomeCheck({ favorites }) {
 
     return (
         <>

ed26ed5a8825e812af958b54d5e6eb98d36924a3
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-18 20:08:23 -0500

    Finished initial implementation of front end

--- resources/js/components/HomeCheck.jsx
+++ resources/js/components/HomeCheck.jsx
@@ -9,7 +9,28 @@ function HomeCheck({ weather, favorites }) {
      */
     const [error, setError] = useState(null);
 
+    /**
+     * State to track if current location is a favorite city.
+     * @type {[boolean, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [isFavorite, setIsFavorite] = useState(false);
+
     const [isHome, setIsHome] = useState(false);
+
+    /**
+     * Determines if a city is a favorite location.  If so we do not render 
+     * form for adding city as favorite.
+     */
+    const isFavoriteCity = () => {
+        const weatherCity = weather.city.toLowerCase();
+        for(let i = 0; i < favorites?.length; i++) {
+            let favCity = favorites[i].name.toLowerCase();
+            if(favCity.includes(weatherCity) || weatherCity.includes(favCity)) {
+                setIsFavorite(true);
+                break;
+            }
+        }
+    }
 
     const isHomeCity = () => {
         const weatherCity = weather.city.toLowerCase();
@@ -24,12 +45,52 @@ function HomeCheck({ weather, favorites }) {
         }
     }
 
+    async function handleSubmit(e) {
+        const storedWeather = weather.readStorage();
+        e.preventDefault();
+        try {
+            const payload = {
+                name: e.target.value.is_home,
+                csrf_token: Forms.CSRFToken(e)
+            }
+            const json = await apiPost("/favorites/store", payload);
+            window.location.reload();
+        } catch (err) {
+            setError(apiError(err));
+        }
+      
+    }
+
     useEffect(() => {
+        isFavoriteCity();
         isHomeCity();
-    })
+    });
+
     return (
         <>
-
+            {isFavorite && (
+                <div>
+                    {isHome ? (
+                        <button className="btn btn-success btn-sm">
+                            <i className="me-2 fa fa-home" aria-hidden="true"></i>Home
+                        </button>
+                    ) : (
+                        <>
+                            {error ? (<p className="text-danger">{error}</p>) : 
+                                <form method="POST" onSubmit={handleSubmit}>
+                                    <Forms.CSRFInput />
+                                    <Forms.Hidden name="is_home" value={weather.getCityInfo()} />
+                                    <button 
+                                        type="submit" 
+                                        className="btn btn-primary btn-sm">
+                                        <i className="me-2 fa fa-home"></i>Set Home
+                                    </button>
+                                </form>
+                            }
+                        </>
+                    )}
+                </div>
+            )}
         </>
     );
 }        

c0686deac9661f5e58b0bef8280df095ae0992fd
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-18 19:50:18 -0500

    Detect if city is home and print home to console if true

--- resources/js/components/HomeCheck.jsx
+++ resources/js/components/HomeCheck.jsx
@@ -2,11 +2,34 @@ import React, { useEffect, useState } from "react";
 import Forms from "@chappy/components/Forms";
 import { apiPost, apiError } from "@chappy/utils/api";
 
-function HomeCheck({ favorites }) {
+function HomeCheck({ weather, favorites }) {
+    /**
+     * Sets value of error if exists.
+     * @type {[string, import('react').Dispatch<import('react').SetStateAction<string>>]}
+     */
+    const [error, setError] = useState(null);
 
+    const [isHome, setIsHome] = useState(false);
+
+    const isHomeCity = () => {
+        const weatherCity = weather.city.toLowerCase();
+        for(let i = 0; i < favorites?.length; i++) {
+            let homeCity = favorites[i].name.toLowerCase();
+
+            if((homeCity.includes(weatherCity) || weatherCity.includes(homeCity)) && favorites[i].is_home == 1) {
+                console.log("home");
+                setIsHome(true);
+                break;
+            }
+        }
+    }
+
+    useEffect(() => {
+        isHomeCity();
+    })
     return (
         <>
-        
+
         </>
     );
 }        
